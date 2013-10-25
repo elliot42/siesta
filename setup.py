b2f21628ce32c8367c0744a2b422d20f56bd6e0d
Elliot Block <elliot@deck36.net>
2013-10-25 16:27:42 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='siesta',
+      version='0.1.0',
+      description='Functional, RESTful web framework',
+      author='Elliot Block',
+      author_email='elliot@deck36.net',
+      url='http://github.com/elliot42/siesta',
+      packages=['siesta'],
+     )
