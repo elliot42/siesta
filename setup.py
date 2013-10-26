d958fcf3ca40739be2a3fbe29666e8e36f56bce2
Elliot Block <elliot@deck36.net>
2013-10-25 17:28:13 -0700

    Include siesta.application package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='siesta',
       author='Elliot Block',
       author_email='elliot@deck36.net',
       url='http://github.com/elliot42/siesta',
-      packages=['siesta'],
+      packages=['siesta', 'siesta.application'],
       install_requires=[
           'werkzeug',
       ],
