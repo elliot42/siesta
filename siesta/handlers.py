e3ba929711d4d72b7f229f5f9dad5e4657b742be
Elliot Block <elliot@deck36.net>
2013-10-27 01:15:13 -0700

    Add Jinja handler
    This allows us to use templates responses out of the box.

--- siesta/handlers.py
+++ siesta/handlers.py
@@ -4,3 +4,16 @@ def html_handler(request_handler):
     def handler(request):
         return (200, [('Content-type', 'text/html')], request_handler(request))
     return handler
+
+def jinja_handler(template, data_provider):
+    """Returns a request handler
+
+    Requires a Jinja2 template, and a `data_provider` function
+    that, given a request, returns a dict of bindings appropriate
+    to fill in the template."""
+    def handler(request):
+        bindings = data_provider(request)
+        return (200,
+                [('Content-Type', 'text/html')],
+                template.render(**bindings))
+    return handler
