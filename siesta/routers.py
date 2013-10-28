066110e4330ab13b76d9939d2b1d006d6edc5c3b
Elliot Block <elliot@deck36.net>
2013-10-27 19:44:39 -0700

    Add NestedResourceRouter
    This commit adds routes.NestedResourceRouter.  It gives you a
    convenient way to define a router in terms of nested resources:
    
        routes = (index_handler, {
            '0.1.0': (None, {
                'dogs': (dogs_handler, {
                    '*': dog_handler,
                 }),
                'cats': (None, {
                    '*': cat_handler,
                }),
            })
        })
    
        router = routes.NestedResourceRouter(routes)
    
    The routes format is defined as a recursive 2-tuple, starting
    from the root of a path:
    
        route = (handler_for_root_resource, dict_of_child_routes)
    
    For exmaple:
    
        child0_routes = (child0_handler, {}) # child has no children
        child1_routes = (child1_handler, {}) # child has no children
    
        (root_handler, {
                'child0': child0_routes,
                'child1': child1_routes,
            })
    
    There is one bit of syntatic sugar, which is that passes a non-tuple
    as a route is equivalent to an empty 2-tuple.  The following
    are equivalent:
    
      routes0 = root_handler
      router0 = NestedResourceRouter(routes0)
    
      routes1 = (root_handler, {})
      router1 = NestedResourceRouter(routes1)
    
    This router is partially formal RESTful, and partially convenience.
    Specifically:
    
    - REST has the idea of resources, upon which one operates with
      HTTP verbs.  This router gives you an easy way to designate
      resources, and map them verb-oriented handlers (specifically
      routers.method_router / routers.resource_descriptor_router).
      It takes you away from the idea of path matching, regexes,
      crazy DSLs.
    
    - REST doesn't actually specify the idea of resource collections vs.
      resource items; they're all just resources--some resources just
      have different behavior than other resources when operated on.
    
    - In practice the simplest way to think about resources and
      routing appears to be in terms of collections and item, so we're
      going to make the common case straightforward.
    
    As usual, a router is really just a routing handler; it's a function
    that, given a request, identifies some other request handling function
    and delegates handling the request to that handler.  You can replace
    this with any other variation of router that you want--it's just
    a thing that figures out how to find and delegate to a handler.

--- siesta/routers.py
+++ siesta/routers.py
@@ -87,3 +87,107 @@ def resource_descriptor_router(resource_handler_descriptor_klass):
                if hasattr(resource_handler_descriptor, method_name.lower())
              }
     return method_router(routes)
+
+class NestedResourceRouter:
+    """Takes a route specification, and returns a handler that will
+    route according to that specification.
+
+    This breaks down a request URL by slash ('/') delimited:
+
+    /
+    /0.1.0
+    /0.1.0/dogs
+    /0.1.0/dogs/20
+
+    And considers each path component a Resource, which can either
+    be handled itself, or checked for if the request wants a child
+    of the resource.
+
+    Route specification is defined as either:
+
+        resource_handler                   # Handler
+        (resource_handler, child_handlers) # 2-typle of handler, children
+
+    `child_handlers` is a `dict`:
+
+        {
+            <str:child_resource_name>: route_specification,
+        }
+
+    For example, a route specification looks like:
+
+        (IndexHandler, {
+             '0.1.0': (None, {
+                 'dogs': (DogsHandler, {
+                     '*': DogHandler,
+                  })
+             })
+         })
+
+    This guy is a callable; an instance acts like a function.
+    """
+    @classmethod
+    def match_route(cls, path_list, routes):
+        """Given a broken down path_component list from a request, and
+        a routes specification, return the appropriate handler
+
+        Example:
+
+            routes = (IndexHandler, {
+                 '0.1.0': (None, {
+                     'dogs': (DogsHandler, {
+                         '*': DogHandler,
+                      })
+                 })
+             })
+
+            path_list = ['0.1.0', 'dogs', '20']
+
+            match_route(path_list, routes)
+            # => DogHandler
+
+        Complies with the basic rules specified above for route
+        specifications.
+        """
+
+        # recusively advance down path_list, trying to match
+        # up available routes against the resource currently
+        # at the head of the path_list
+        if len(path_list) == 0:
+            # end of the path list; either what we're looking
+            # for is here or it's not present at all.
+            if routes:
+                if type(routes) is not tuple:
+                    return routes # routes is itself a handler
+                elif type(routes) is tuple:
+                    handler, child_handlers = routes
+                    if handler:
+                        return handler
+            return handlers.passthrough(responses.not_found())
+        else:
+            # there are still items in the path list; the
+            # request is for an item further down the `routes` tree;
+            # try to find the next level deeper and recur.
+            child_routes = routes[1]
+            head, rest = path_list[0], path_list[1:]
+
+            if head in child_routes:
+                return cls.match_route(rest, child_routes[head])
+            elif '*' in child_routes: # the wildcard route; matches anything.
+                return cls.match_route(rest, child_routes['*'])
+            return handlers.passthrough(responses.not_found())
+
+    def __init__(self, routes):
+        """Given a route specification, this router (self) up
+        to find handlers according to the route specification."""
+        self.routes = routes
+
+    def __call__(self, request):
+        """This object acts like a function, specifically a handler;
+        When called with a request, it finds the appropriate handlers
+        from its initial specification and then delegates generating
+        the response to the found handler."""
+        parse_result = urlparse(request.url)
+        path_list = [p for p in parse_result.path.split('/') if p]
+        resource_handler = self.match_route(path_list, self.routes)
+        return resource_handler(request)
--- tests/router_tests.py
+++ tests/router_tests.py
@@ -71,3 +71,155 @@ def test_resource_descriptor_router_post():
     c = Client(app, BaseResponse)
     resp = c.post('/')
     assert resp.status_code == 200
+
+def test_nested_resource_router_root():
+    """Test that it hits the root handler"""
+    index_handler = handlers.passthrough(responses.ok)
+    ok = handlers.passthrough(responses.ok)
+
+    routes = (index_handler, {
+                  '0.1.0': (None, {
+                      'dogs': (ok, {
+                          '*': ok,
+                       })
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/')
+    assert resp.status_code == 200
+
+def test_nested_resource_router_nested_resource():
+    "Test that it can hit a nested resource"""
+    index_handler = handlers.passthrough(responses.ok)
+    dogs_handler = handlers.html_handler(lambda request: 'dogs!')
+    dog_handler = handlers.html_handler(lambda request: 'holla!')
+
+    routes = (index_handler, {
+                  '0.1.0': (None, {
+                      'dogs': (dogs_handler, {
+                          '*': dog_handler,
+                       })
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0/dogs')
+    assert ''.join(resp.response) == 'dogs!'
+
+def test_nested_resource_router_proper_nested_resource():
+    """Test that it can hit a proper nested resource, choosing
+    between several available at the level"""
+    def html_passthrough_handler(content):
+        return handlers.html_handler(lambda request: content)
+
+    dogs_handler = html_passthrough_handler('dogs!')
+    cats_handler = html_passthrough_handler('cats!')
+    hogs_handler = html_passthrough_handler('hogs!')
+
+    routes = (None, {
+                  '0.1.0': (None, {
+                      'dogs': dogs_handler,
+                      'cats': cats_handler,
+                      'hogs': hogs_handler,
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0/cats')
+    assert ''.join(resp.response) == 'cats!'
+
+
+def test_nested_resource_router_wildcard_resource():
+    "Test that it can hit a wildcard resource"""
+    index_handler = handlers.passthrough(responses.ok)
+    dogs_handler = handlers.html_handler(lambda request: 'dogs!')
+    dog_handler = handlers.html_handler(lambda request: 'holla!')
+
+    routes = (index_handler, {
+                  '0.1.0': (None, {
+                      'dogs': (dogs_handler, {
+                          '*': dog_handler,
+                       })
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0/dogs/20')
+    assert ''.join(resp.response) == 'holla!'
+
+def test_nested_resource_router_404_no_route():
+    "Test that it 404s on a missing route"""
+    index_handler = handlers.passthrough(responses.ok)
+    dogs_handler = handlers.passthrough(responses.ok)
+    dog_handler = handlers.html_handler(lambda request: 'holla!')
+
+    routes = (index_handler, {
+                  '0.1.0': (None, {
+                      'dogs': (dogs_handler, {
+                          '*': dog_handler,
+                       })
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0/cats')
+    assert resp.status_code == 404
+
+def test_nested_resource_router_404_none_route():
+    "Test that it 404s on a None route"""
+    index_handler = handlers.passthrough(responses.ok)
+    dogs_handler = handlers.passthrough(responses.ok)
+    dog_handler = handlers.html_handler(lambda request: 'holla!')
+
+    routes = (index_handler, {
+                  '0.1.0': (None, {
+                      'dogs': (dogs_handler, {
+                          '*': dog_handler,
+                       })
+                  })
+              })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0')
+    assert resp.status_code == 404
+
+def test_nested_resource_router_subwildcard():
+    "Test that it can hit a named route beneath a wildcard"""
+
+    ok = handlers.passthrough(responses.ok)
+
+    routes = (None, {
+                 '0.1.0': (None, {
+                      'dogs': (None, {
+                          '*': (None, {
+                              'fur': ok,
+                          }),
+                      })
+                  })
+             })
+
+    router = routers.NestedResourceRouter(routes)
+    app = siesta.application.application(router)
+
+    c = Client(app, BaseResponse)
+    resp = c.post('/0.1.0/dogs/40/fur')
+    assert resp.status_code == 200
