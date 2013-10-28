14efacb9cd22c12f62843209aeec3e7ddf9400bd
Elliot Block <elliot@deck36.net>
2013-10-27 21:27:21 -0700

    Update example.py to use resource-ful utils

--- example.py
+++ example.py
@@ -1,18 +1,40 @@
 import siesta.application
 import siesta.routers
-from siesta.handlers import html_handler as handle
 
-router = siesta.routers.resource_method_router({
-    '': { 'GET': handle(lambda request: 'Hello world!') },
-    'cats' : {
-        'GET': handle(lambda request: 'Cat GET!!!'),
-        'PUT': handle(lambda request: 'CAT PUT!!!'),
-    },
-    'dogs' : {},
-})
+class IndexHandler(siesta.routers.Resource):
+    def get(self, request):
+        return (200, [('Content-Type', 'text/html')], 'index!')
+
+class CatsHandler(siesta.routers.Resource):
+    def get(self, request):
+        return (200, [('Content-Type', 'text/html')], 'collection of cats!')
+
+class CatHandler(siesta.routers.Resource):
+    def get(self, request):
+        return (200, [('Content-Type', 'text/html')], 'get cat!')
+
+    def put(self, request):
+        return (200, [('Content-Type', 'text/html')], 'put cat!')
+
+class DogHandler(siesta.routers.Resource):
+    def get(self, request):
+        return (200, [('Content-Type', 'text/html')], 'get dog!')
+
+router = siesta.routers.NestedResourceRouter(
+    (IndexHandler(), {
+        'cats': (CatsHandler(), {
+            '*': CatHandler(),
+        }),
+        'dogs': (None, {
+            '*': DogHandler()
+        }),
+    }))
 
 app = siesta.application.application(router)
 
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
     run_simple('localhost', 4000, app)
+
+# curl http://localhost:4000/cats/15
+# => 'one cat!'
